a6a236a6d273fb3453f9b6fad84fdb9bfdc69a6c
Asus <Asus@anteng>
2023-10-02 16:02:08 +0700

    TUGAS

---
+++ PratikumAlPro_2311533011/src/Pekan1/HelloWorld.java
@@ -0,0 +1,7 @@
+package Pekan1;
+
+public class HelloWorld {
+	public static void main(String [] args) {
+		System.out.println("HelloWorld");
+	}
+}
---
+++ PratikumAlPro_2311533011/src/RadjasaAryaPradifta_2311533011/RadjasaAryaPradifta_2311533011.java
@@ -0,0 +1,12 @@
+package RadjasaAryaPradifta_2311533011;
+
+public class RadjasaAryaPradifta_2311533011 {
+	public static void main(String[] args) {
+        String nama = "RadjasaAryaPradifta";
+        String nim = "2311533011";
+        String output = nama + "_" + nim;
+        
+        System.out.println(output);
+    }
+}
+
